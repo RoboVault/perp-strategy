564889380a8f681086c8e7d8c7d0864a28eb43f5
iHeroN <iherondev@gmail.com>
2023-01-28 18:42:26 +0100

    Porting main files

---
+++ contracts/entry/optimism/WETHPERP.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+pragma experimental ABIEncoderV2;
+
+import "../../CoreStrategyPerp.sol";
+import {
+    SafeERC20,
+    IERC20,
+    Address
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract WETHPERP is BaseStrategy {
+    using SafeERC20 for IERC20;
+    uint256 constant farmPid = 0;
+
+    constructor(address _vault)
+        public
+        CoreStrategyPerp(
+            _vault,
+            CoreStrategyPerpConfig(
+                0x7F5c764cBc14f9669B88837ca1490cCa17c31607, // want -> USDC 
+                0x4200000000000000000000000000000000000006, // short -> WETH
+                0xE6Df0BB08e5A97b40B21950a0A51b94c4DbA0Ff6, // router
+                1e4, //mindeploy
+                0xAD7b4C162707E0B2b5f6fdDbD3f8538A5fbA0d60, // Perp Vault
+                0x8C835DFaA34e2AE61775e80EE29E2c724c6AE2BB // vETH
+            )
+        )
+    {}
+
+    function balancePendingHarvest() public view override returns (uint256) {
+        // uint256 pending =
+        //     IZipRewards(farmMasterChef)
+        //         .pendingReward(farmPid, address(this))
+        //         .add(farmToken.balanceOf(address(this)));
+        // uint256 harvestLp_A = farmToken.balanceOf(address(farmTokenLP));
+        // uint256 shortLP_A = _getShortInHarvestLp();
+        // uint256 totalShort = pending.mul(shortLP_A).div(harvestLp_A);
+        // (uint256 wantLP_B, uint256 shortLP_B) = getLpReserves();
+        // return totalShort.mul(wantLP_B).div(shortLP_B);
+    }
+
+    function _pendingRewards() internal view returns (uint256) {
+        return 0; // TODO
+    }
+
+    function _depositLp() internal override {
+        // uint256 lpBalance = wantShortLP.balanceOf(address(this));
+
+        // IZipRewards(farmMasterChef).deposit(
+        //     farmPid,
+        //     uint128(lpBalance),
+        //     address(this)
+        // );
+    }
+
+    function _withdrawFarm(uint256 _amount) internal override {
+        if (_amount > 0)
+            IZipRewards(farmMasterChef).withdraw(
+                farmPid,
+                uint128(_amount),
+                address(this)
+            );
+    }
+
+    function claimHarvest() internal override {
+        IZipRewards(farmMasterChef).harvest(farmPid, address(this));
+    }
+
+    function countLpPooled() internal view override returns (uint256) {
+        return
+            IZipRewards(farmMasterChef).userInfo(farmPid, address(this)).amount;
+    }
+}
---
+++ contracts/lib/PerpFixedPoint96.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
+
+library PerpFixedPoint96 {
+    int256 internal constant _IQ96 = 0x1000000000000000000000000;
+}
